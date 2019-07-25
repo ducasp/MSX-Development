64a014ba4352c4011d9a77cf04e2209a7e67d091
ducasp <ducasp@users.noreply.github.com>
2019-07-25 19:43:20 -0300

    Delete makesntp.cmd

--- UNAPI/SNTP/makesntp.cmd
+++
@@ -1,2 +0,0 @@
-sdcc --code-loc 0x180 --data-loc 0 -mz80 --disable-warning 196 --no-std-crt0 crt0_msxdos_advanced.rel fusion-c\include\printf_simple.rel fusion-c\include\putchar.rel fusion.lib asm.lib sntp.c
-hex2bin -e com sntp.ihx
